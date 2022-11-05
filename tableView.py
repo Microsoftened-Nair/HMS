8f7d846a8ab4cf61454e5594a9fd8f0af5df5060
Microsoftened-Nair <nairmeghraj@gmail.com>
2022-11-05 20:28:05 +0530

    tableView.py works

--- tableView.py
+++ tableView.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
 import mysql.connector
 
 db = mysql.connector.connect(
@@ -10,6 +11,44 @@ db = mysql.connector.connect(
 
 c = db.cursor()
 
+c.execute("describe pms")
+myresult = c.fetchall()
+
+data = []
+data2 = []
+for x in myresult:
+    data2.append(x[0])
+
+tup = tuple(data2)
+data.append(tup)
+
+c.execute("select * from pms")
+myresult = c.fetchall()
+
+for i in myresult:
+    data.append(i)
+print(len(data) - 1)
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -20,6 +59,8 @@ class Ui_MainWindow(object):
         self.tableView = QtWidgets.QTableView(self.centralwidget)
         self.tableView.setGeometry(QtCore.QRect(20, 10, 751, 561))
         self.tableView.setObjectName("tableView")
+        self.model = TableModel(data)
+        self.tableView.setModel(self.model)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
